a76d6bc00559eff40e61214e82bc4f29db3be7a4
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2024-12-13 17:06:54 +0200

    autoloading

--- lib/essence.rb
+++ lib/essence.rb
@@ -4,6 +4,7 @@ module Essence
   # Autoloading
   autoload :Component, "essence/component"
 
+  autoload :Button, "essence/components/button"
   autoload :Link, "essence/components/link"
   autoload :Skeleton, "essence/components/skeleton"
   autoload :Row, "essence/components/row"
