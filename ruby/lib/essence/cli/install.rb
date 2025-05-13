f59d4bc2033431cafce3af7da8eb72eae29a09ef
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2025-05-13 13:27:01 +0300

    Add support for installing the tailwind_merge gem
    Introduce methods to check for and add the tailwind_merge gem.
    Provide feedback on success or failure during gem installation.

--- ruby/lib/essence/cli/install.rb
+++ ruby/lib/essence/cli/install.rb
@@ -11,7 +11,44 @@ module Essence
       def call(*)
         puts "[Essence UI] Installing..."
         ::Essence::CLI::Add.new.call(*[{component: "essence"}])
+
+
+        add_gem("tailwind_merge") unless has_gem?("tailwind_merge")
+
+        # begin
+        #   system(*command)
+        #   if $?.success?
+        #     puts "✅ Successfully added gem 'tailwind_merge' to your Gemfile"
+        #   else
+        #     puts "❌ Failed to add gem 'tailwind_merge' to your Gemfile (exit code: #{$?.exitstatus})"
+        #   end
+        # rescue => e
+        #   puts "❌ Error: #{e.message}"
+        # end
+
         puts "[Essence UI] Done!"
+      end
+
+      private
+
+      def has_gem?(title)
+        Bundler.load.specs.find { |s| s.name == title }
+      end
+
+      def add_gem(title)
+        puts "[Essence UI] Adding #{title} gem"
+        command = ["bundle", "add", title]
+
+        begin
+          system(*command)
+          if $?.success?
+            puts "[Essence UI] Successfully added gem 'tailwind_merge' to your Gemfile"
+          else
+            puts "[Essence UI] Failed to add gem 'tailwind_merge' to your Gemfile (exit code: #{$?.exitstatus})"
+          end
+        rescue => e
+          puts "[Essence UI] Error: #{e.message}"
+        end
       end
     end
   end
