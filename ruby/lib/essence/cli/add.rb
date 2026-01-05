e1feaddd73e23dcfed90d843454400883c23ac88
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2026-01-05 21:44:30 +0200

    Update add.rb

--- ruby/lib/essence/cli/add.rb
+++ ruby/lib/essence/cli/add.rb
@@ -32,28 +32,55 @@ module Essence
         case artifact["kind"]
         when "phlex", "stimulus"
           insert_file(artifact:)
-        when "node_package"
-          # TODO
+        when 'node_package'
+          install_package(artifact['name']) if artifact['source'] == 'npm'
         end
       end
 
       def insert_file(artifact:)
-        destination_path = build_destination_path(kind: artifact["kind"], slug:)
+        destination_path = build_destination_path(kind: artifact['kind'], slug:)
         return unless destination_path
 
         ::FileUtils.mkdir_p(destination_path.dirname)
-        ::File.write(destination_path, artifact.dig("content"))
+        ::File.write(destination_path, artifact.dig('content'))
         puts "[Essence UI] Adding #{destination_path}"
       end
 
       def build_destination_path(kind:, slug:)
         case kind
-        when "phlex"
+        when 'phlex'
           ::Essence.configuration.phlex_components_dir.join("#{slug}.rb")
-        when "stimulus"
+        when 'stimulus'
           ::Essence.configuration.stimulus_controllers_dir.join("#{slug}_controller.js")
+        end
+      end
+
+      def detect_package_manager
+        if File.exist?('yarn.lock')
+          :yarn
+        elsif File.exist?('bun.lock') || File.exist?('bun.lockb')
+          :bun
+        elsif File.exist?('package-lock.json')
+          :npm
+        elsif File.exist?('config/importmap.rb')
+          :importmap
         else
-          nil
+          :npm
+        end
+      end
+
+      def install_package(name)
+        manager = detect_package_manager
+        puts "[Essence UI] Installing #{name} via #{manager}"
+        case manager
+        when :yarn
+          system("yarn add #{name}")
+        when :bun
+          system("bun add #{name}")
+        when :npm
+          system("npm install #{name}")
+        when :importmap
+          system("bin/rails importmap:pin #{name}")
         end
       end
     end
